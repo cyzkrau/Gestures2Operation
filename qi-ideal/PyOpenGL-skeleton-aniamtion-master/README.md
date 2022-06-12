afe0681124bbb0a30b8c35791f814674db7047df
Vermillion-de <2308224300@qq.com>
2022-06-12 15:02:15 +0800

    22-06-12-Add a framework maybe used for animation

---
+++ qi-ideal/PyOpenGL-skeleton-aniamtion-master/README.md
@@ -0,0 +1,15 @@
+# PyOpenGL-skeleton-aniamtion
+OpenGL skeletal animation using PyOpenGL 
+
+Blender Tutorials by Sebastian Lague
+https://www.youtube.com/playlist?list=PLFt_AvWsXl0fEx02iXR8uhDsVGhmM9Pse
+
+Skeletal Animation Tutorials by TheThinMatrix
+https://github.com/TheThinMatrix/OpenGL-Animation
+
+PyOpenGL-skeleton-aniamtion csdn blog page
+https://blog.csdn.net/HelloWorld1108/article/details/89228893
+
+##### Animation gif
+
+![aniamtion](https://github.com/xing-shuai/PyOpenGL-skeleton-aniamtion/blob/master/run.gif)
---
+++ qi-ideal/gesture_geo.py
@@ -0,0 +1,79 @@
+import cv2
+from cv2 import RETR_LIST
+import cvzone
+from cvzone.HandTrackingModule import HandDetector
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+
+detector = HandDetector(detectionCon=0.8)
+
+cx, cy, w, h = 200, 200, 200, 200
+color = (150, 0, 150)
+
+
+def distant(A, B):
+    return abs(A[0] - B[0]) + abs(A[1] - B[1])
+
+
+class Obstacle():
+
+    def __init__(self,
+                 posCenter,
+                 size=[200, 200],
+                 type="rectangle",
+                 color=(100, 0, 100)):
+        self.posCenter = posCenter
+        self.size = size
+        self.type = type
+        self.color = color
+
+    def update(self, img, cursor1, cursor2, cursor3):
+        color = self.color
+        if self.posCenter[0] - self.size[0] // 2 < cursor1[
+                0] < self.posCenter[0] + self.size[0] // 2 and self.posCenter[
+                    1] - self.size[1] // 2 < cursor1[
+                        1] << self.posCenter[1] + self.size[1] // 2:
+            color = (100, 0, 0)
+            if distant(cursor_1, cursor_2) < 90:
+                color = (0, 255, 0)
+                self.posCenter = ((cursor1[0] + cursor2[0]) // 2,
+                                  (cursor1[1] + cursor2[1]) // 2)
+        cv2.rectangle(img, (self.posCenter[0] - self.size[0] // 2,
+                            self.posCenter[1] - self.size[1] // 2),
+                      (self.posCenter[0] + self.size[0] // 2,
+                       self.posCenter[1] + self.size[1] // 2), color,
+                      cv2.FILLED)
+        return img
+
+    def draw(self, img):
+        cv2.rectangle(img, (self.posCenter[0] - self.size[0] // 2,
+                            self.posCenter[1] - self.size[1] // 2),
+                      (self.posCenter[0] + self.size[0] // 2,
+                       self.posCenter[1] + self.size[1] // 2), self.color,
+                      cv2.FILLED)
+        return img
+
+
+rectList = []
+for i in range(3):
+    rectList.append(Obstacle((i * 250 + 150, 250)))
+
+while True:
+    sucess, img = cap.read()
+    img = cv2.flip(img, 1)
+    hand, img = detector.findHands(img)
+    if hand:
+        lmList = hand[0]['lmList']
+        cursor_1 = lmList[8]
+        cursor_2 = lmList[12]
+        cursor_3 = lmList[4]
+        for rect in rectList:
+            img = rect.update(img, cursor_1, cursor_2, cursor_3)
+    else:
+        for rect in rectList:
+            img = rect.draw(img)
+
+    cv2.imshow("image", img)
+    cv2.waitKey(1)
